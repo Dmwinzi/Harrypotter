927b602bd430497f137095dc79ef2a61bfa6ffbb
Mwinzi <danielmwinzi150@gmail.com>
2023-04-03 00:44:34 +0300

    Implemented search functionality

--- app/src/main/java/com/example/harrypotter/Presentation/Characters.kt
+++ app/src/main/java/com/example/harrypotter/Presentation/Characters.kt
@@ -1,22 +1,29 @@
 package com.example.harrypotter.Presentation
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -37,6 +44,9 @@ fun Characters(viewModel: Mainviewmodel,navController: NavHostController){
     var characters  = viewModel._characters.collectAsState()
 
     var allcharacters = remember { mutableStateOf(listOf<HarrycharsItem>()) }
+   var searchtext  by remember { mutableStateOf("")}
+   var filteredcharacters by remember { mutableStateOf(listOf<HarrycharsItem>()) }
+   var listoffilteredcharacters by  remember { mutableStateOf(mutableListOf<HarrycharsItem>()) }
 
     when(characters.value){
         is Resource.Success -> {
@@ -53,21 +63,70 @@ fun Characters(viewModel: Mainviewmodel,navController: NavHostController){
     }
 
 
+       if (searchtext.isEmpty()){
+           filteredcharacters  = allcharacters.value
+       } else {
+           filteredcharacters = allcharacters.value.filter {
+               it.actor.contains(searchtext, ignoreCase = true)
+           }
+       }
 
-    //Log.d("characters",characters.toString())
 
-   Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
 
+
+   Scaffold(modifier = Modifier.fillMaxSize(),
+       topBar = {
+           OutlinedTextField(modifier = Modifier
+               .height(50.dp)
+               .fillMaxWidth()
+              ,value = searchtext, onValueChange = {searchtext = it}, placeholder = { Text(text = "Search")}, leadingIcon = {
+               IconButton(onClick = { /*TODO*/ }) {
+                  Icon(painter = painterResource(id = R.drawable.baseline_search_24), contentDescription = null, tint = Color.Blue)
+               }
+           }, trailingIcon = {
+               IconButton(onClick = { searchtext = "" }) {
+                  Icon(painter = painterResource(id = R.drawable.baseline_cancel_24) , contentDescription = null, tint = Color.Red)
+               }
+           },singleLine = true, shape = RoundedCornerShape(15.dp), colors = TextFieldDefaults.outlinedTextFieldColors(
+               focusedBorderColor = Color.Blue,
+               unfocusedBorderColor = Color.Transparent
+           ))
+       }
+   ) { paddingValues ->
     LazyColumn(modifier = Modifier.padding(paddingValues)){
         item {
             Circularprogress(isloading = isloading)
         }
-         items(allcharacters.value){ item ->
+         items(filteredcharacters){ item ->
              Row(modifier = Modifier
                  .fillMaxWidth()
                  .clickable(onClick = {
-                 var character  = Harrycharactersparcelables(item.actor, item.alive, item.alternate_actors, item.alternate_names, item.ancestry, item.dateOfBirth,item.eyeColour, item.gender, item.hairColour, item.hogwartsStaff, item.hogwartsStudent, item.house, item.id, item.image, item.name, item.patronus, item.species, item.wand, item.wizard, item.yearOfBirth)
-                     navController.currentBackStackEntry?.savedStateHandle?.set("characters",character)
+                     var character = Harrycharactersparcelables(
+                         item.actor,
+                         item.alive,
+                         item.alternate_actors,
+                         item.alternate_names,
+                         item.ancestry,
+                         item.dateOfBirth,
+                         item.eyeColour,
+                         item.gender,
+                         item.hairColour,
+                         item.hogwartsStaff,
+                         item.hogwartsStudent,
+                         item.house,
+                         item.id,
+                         item.image,
+                         item.name,
+                         item.patronus,
+                         item.species,
+                         item.wand,
+                         item.wizard,
+                         item.yearOfBirth
+                     )
+                     navController.currentBackStackEntry?.savedStateHandle?.set(
+                         "characters",
+                         character
+                     )
                      navController.navigate(Screens.Characterinfo.route)
                  })){
                  Row(modifier = Modifier
